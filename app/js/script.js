082e0375d5091fa5e763cadacc3f07eae434417c
Lucapandolfelli <lucapandolfelli2002@gmail.com>
2022-06-19 20:06:12 -0300

    Using ternary operators

--- app/js/script.js
+++ app/js/script.js
@@ -3,12 +3,25 @@ Creamos un objeto teniendo en cuenta los parámetros que nos devuelve la API en 
 Link: https://www.pexels.com/es-es/api/documentation/#photos-overview.
 */
 class Photo {
-    constructor(id, url, src, photographer, alt){
+    /* constructor(id, url, src, photographer, alt){
         this.id = id; // Nro identificador de cada foto en particular.
         this.url = url; // URL de la imagen original subida en Pexels.
         this.src = src; // URL de la imagen para ser utilizada en nuestro proyecto.
         this.photographer = photographer; // Nombre del fotografo.
         this.alt = alt; // Texto descriptivo de la imagen el cual usa la API para filtrar y devolver resultados de búsqueda.
+    } */
+    // Creamos el constructor de la clase a partir de las propiedades del objeto dado por un json de una API.
+    constructor (json){
+        Object.assign(this, json);
+    }
+    showModal(){
+        galleryItem.addEventListener('click', () => {
+            Swal.fire({
+                imageUrl: this.src,
+                imageHeight: 500,
+                imageAlt: this.alt
+            });
+        });
     }
 }
 
@@ -18,21 +31,29 @@ const generateId = () => {
     return id;
 }
 
-// Creamos un array que simule la base de datos donde se almacenan las fotos. 
-const photos = [
-    new Photo(generateId(), 'ejemploURL', '/dist/assets/images/caballomontaña.jpg', 'Jose Molina', 'Caballos en montaña'),
-    new Photo(generateId(), 'ejemploURL', '/dist/assets/images/gatosaltando.jpg', 'Ignacio Fernandez', 'Gato saltando'),
-    new Photo(generateId(), 'ejemploURL', '/dist/assets/images/formula1.jpg', 'Martin Caro', 'Autos de Formula 1'),
-    new Photo(generateId(), 'ejemploURL', '/dist/assets/images/buenosaires.jpg', 'Beatriz Quiroga', 'Buenos Aires'),
-    new Photo(generateId(), 'ejemploURL', '/dist/assets/images/personamontaña.jpg', 'Jose Molina', 'Persona en montaña')
+// Creamos un array que simule el json que devuelve una API. Los primeros 8 elementos representan fotos random. El resto representan fotos para ser buscadas.
+const apiPhotos = [
+    {id: generateId(), url: 'ejemploURL', src: '/dist/assets/images/example (1).jpeg', photographer: '', alt: ''},
+    {id: generateId(), url: 'ejemploURL', src: '/dist/assets/images/example (2).jpeg', photographer: '', alt: ''},
+    {id: generateId(), url: 'ejemploURL', src: '/dist/assets/images/example (3).jpeg', photographer: '', alt: ''},
+    {id: generateId(), url: 'ejemploURL', src: '/dist/assets/images/example (4).jpeg', photographer: '', alt: ''},
+    {id: generateId(), url: 'ejemploURL', src: '/dist/assets/images/example (5).jpeg', photographer: '', alt: ''},
+    {id: generateId(), url: 'ejemploURL', src: '/dist/assets/images/example (6).jpeg', photographer: '', alt: ''},
+    {id: generateId(), url: 'ejemploURL', src: '/dist/assets/images/example (7).jpeg', photographer: '', alt: ''},
+    {id: generateId(), url: 'ejemploURL', src: '/dist/assets/images/example (8).jpeg', photographer: '', alt: ''},
+    {id: generateId(), url: 'ejemploURL', src: '/dist/assets/images/caballomontaña.jpg', photographer: 'Jose Molina', alt: 'Caballos en montaña'},
+    {id: generateId(), url: 'ejemploURL', src: '/dist/assets/images/gatosaltando.jpg', photographer: 'Ignacio Fernandez', alt: 'Gato saltando'},
+    {id: generateId(), url: 'ejemploURL', src: '/dist/assets/images/formula1.jpg', photographer: 'Martin Caro', alt: 'Autos de Formula 1'},
+    {id: generateId(), url: 'ejemploURL', src: '/dist/assets/images/buenosaires.jpg', photographer: 'Beatriz Quiroga', alt: 'Buenos Aires'},
+    {id: generateId(), url: 'ejemploURL', src: '/dist/assets/images/personamontaña.jpg', photographer: 'Jose Molina', alt: 'Persona en montaña'}
 ];
 
 let errorAlert = document.getElementById('errorAlert');
-let inputSearch = document.querySelector('input');
 let clearInputButton = document.getElementById('clearInput');
 let searchForm = document.getElementById('searchForm');
 let gallery = document.getElementById('gallery');
 let galleryContainer = document.getElementById('galleryContainer');
+let galleryItem = document.getElementsByClassName('gallery__item');
 
 // Creamos una función que nos permita validar si lo que ingresa el usuario cumple el requisito especificado. Si no lo cumple devuelve 'false', y si lo cumple devuelve 'true'. 
 const validateSearch = (word) => {
@@ -53,13 +74,15 @@ const removeAllChild = (parent) => {
 }
 
 // Creamos una función que nos permita crear nuevos elementos (fotos) para la galería.
-const createNewGalleryItem = (galleryTitle) => {
+const createNewGalleryItem = (photoObject) => {
+    // Creamos un nuevo objeto 'Photo' a partir del objeto que nos dá el 'json'.
+    const photo = new Photo(photoObject);
     // Creamos un nuevo elemento article.
     let newGalleryItem = document.createElement('article');
     // Le asignamos la clase que define los estilos que poseerán las fotos.
     newGalleryItem.classList.add('gallery__item');
-    // Y le ponemos lo que el usuario quiera mostrar.
-    newGalleryItem.innerHTML = galleryTitle;
+    // Y le ponemos una etiqueta img con los datos del objeto para que se muestren.
+    newGalleryItem.innerHTML = `<img src="${photo.src}" alt="${photo.alt}"></img>`;
     // Y por último le agregamos ese nuevo elemento a la galería. Utilicé el 'prepend' ya que quiero que se agregue al principio, para respetar el orden que simulo en el array.
     galleryContainer.prepend(newGalleryItem);
 }
@@ -67,32 +90,31 @@ const createNewGalleryItem = (galleryTitle) => {
 // Creamos una función que nos permita saber si lo que ingresa el usuario coincide de alguna forma con el atributo 'Alt' de alguna foto. 
 const filterPhotos = (search) => {
     // Filtramos el array de objetos para ver si la palabra buscada se encuentra en el alt de la foto. 
-    let filteredPhotos = photos.filter(photo => photo.alt.toLowerCase().includes(search));
+    let filteredPhotos = apiPhotos.filter(photo => photo.alt.toLowerCase().includes(search));
     /* 
     Array.filter() devuelve un array nuevo si se cumple la condición y uno vacío si no se cumple. 
     Comprobamos si el array está vacío, es decir, que la plabra buscada no matcheó con ningún objeto de nuestro array(simulador de base de datos). 
     */
-    if (filteredPhotos.length == 0){
+    if (filteredPhotos.length === 0){
         // Mostramos que ocurrió un error mediante un h2.
         gallery.innerHTML = `<h2>No se pudo encontrar ninguna foto relacionada a "${search}".</h2>`;
     } else{
         // Eliminamos todas las fotos de la gelería.
         removeAllChild(galleryContainer);
-
         // Aplicando la función 'forEach' de orden superior. Por cada foto filtrada devolvemos un nuevo elemento para la galería.
         filteredPhotos.forEach((photo) => {
-            // Llamamos la función que crea un nuevo elemento de la galería.
-            createNewGalleryItem(`<img src="${photo.src}" alt="${photo.alt}"></img>`);
+            // Llamamos la función que crea un nuevo elemento de la galería. 'photo' simula un objeto de un json que nos devuelve la API.
+            createNewGalleryItem(photo);
         });
     }
 }
 
 // Creamos una función que al cargarse la página simule que se trajeron fotos aleatoriamente. La misma cargará 8 fotos.
-const getInitialRandomPhotos = () => {
-    for (let i=1; i <= 8; i++){
-        // Llamamos la función que crea un nuevo elemento de la galería.
-        createNewGalleryItem(`<img src="/dist/assets/images/example (${i}).jpeg" alt="Foto(${i})"></img>`);
-    }
+const getInitialRandomPhotos = (photosArray) => {
+    let firstEightItems = photosArray.slice(0, 8);
+    firstEightItems.forEach((photo) => {
+        createNewGalleryItem(photo);
+    });
 }
 
 // Eveneto submit del form, donde se llama a una función que trae las fotos a partir de la búsqueda.
@@ -104,9 +126,10 @@ searchForm.addEventListener('submit', (e) => {
 clearInputButton.addEventListener('click', () => {
     document.getElementById('searchInput').value = '';
     removeAllChild(galleryContainer);
-    getInitialRandomPhotos();
+    getInitialRandomPhotos(apiPhotos);
     errorAlert.innerText = '';
 });
+
 
 // Creamos una función que nos permita tomar lo que el usuario ingresa en la barra de búsqueda y fijarnos si matchea con alguna de las fotos que tenemos guardadas en el array (simulador de base de datos).
 const getSearchedPhotos = (e) => {
@@ -129,7 +152,7 @@ const getSearchedPhotos = (e) => {
 
 // Cuando se carge la ventana, llamamos a la función que nos carga 8 fotos simulando que son fotos aleatorias.
 window.addEventListener('load', () => {
-    getInitialRandomPhotos();
+    getInitialRandomPhotos(apiPhotos);
 });
 
 // Toggle theme functionality
@@ -152,20 +175,12 @@ const disableDarkTheme = () => {
     ball.style.transform = 'translateX(0)';
 }
 
-if (theme === 'darkTheme'){
-    enableDarkTheme();
-}else{
-    disableDarkTheme();
-}
+theme === 'darkTheme' ? enableDarkTheme() : disableDarkTheme();
 
 // Cuando se aplique el evento click al botón toggle, si el local storage 'theme' no es 'darkTheme', lo habilitamos y sino lo deshabilitamos.
 toggleTheme.addEventListener('click', () => {
     // Actualizamos el local storage cuando se hace click.
     theme = localStorage.getItem('theme');
-    if (theme != 'darkTheme'){
-        enableDarkTheme();
-    }else{
-        disableDarkTheme();
-    }
+    theme != 'darkTheme' ? enableDarkTheme() : disableDarkTheme();
 });
 
